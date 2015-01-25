7d6ffb1f6b9ca4207c90b0b2de22b1b3953f1def
Aric Rosenbaum <aric_rosenbaum@yahoo.com>
2015-01-25 18:46:37 -0500

    Initial commit

---
+++ codebook.md
@@ -0,0 +1,85 @@
+# Code Book
+
+## ID Fields
+* activity - The type of activity
+* subjectID - The subject of the observation
+
+## Data fields
+* timeBodyAcc-mean()-X
+* timeBodyAcc-mean()-Y
+* timeBodyAcc-mean()-Z
+* timeBodyAcc-std()-X
+* timeBodyAcc-std()-Y
+* timeBodyAcc-std()-Z
+* timeGravityAcc-mean()-X
+* timeGravityAcc-mean()-Y
+* timeGravityAcc-mean()-Z
+* timeGravityAcc-std()-X
+* timeGravityAcc-std()-Y
+* timeGravityAcc-std()-Z
+* timeBodyAccJerk-mean()-X
+* timeBodyAccJerk-mean()-Y
+* timeBodyAccJerk-mean()-Z
+* timeBodyAccJerk-std()-X
+* timeBodyAccJerk-std()-Y
+* timeBodyAccJerk-std()-Z
+* timeBodyGyro-mean()-X
+* timeBodyGyro-mean()-Y
+* timeBodyGyro-mean()-Z
+* timeBodyGyro-std()-X
+* timeBodyGyro-std()-Y
+* timeBodyGyro-std()-Z
+* timeBodyGyroJerk-mean()-X
+* timeBodyGyroJerk-mean()-Y
+* timeBodyGyroJerk-mean()-Z
+* timeBodyGyroJerk-std()-X
+* timeBodyGyroJerk-std()-Y
+* timeBodyGyroJerk-std()-Z
+* timeBodyAccMag-mean()
+* timeBodyAccMag-std()
+* timeGravityAccMag-mean()
+* timeGravityAccMag-std()
+* timeBodyAccJerkMag-mean()
+* timeBodyAccJerkMag-std()
+* timeBodyGyroMag-mean()
+* timeBodyGyroMag-std()
+* timeBodyGyroJerkMag-mean()
+* timeBodyGyroJerkMag-std()
+* freqBodyAcc-mean()-X
+* freqBodyAcc-mean()-Y
+* freqBodyAcc-mean()-Z
+* freqBodyAcc-std()-X
+* freqBodyAcc-std()-Y
+* freqBodyAcc-std()-Z
+* freqBodyAccJerk-mean()-X
+* freqBodyAccJerk-mean()-Y
+* freqBodyAccJerk-mean()-Z
+* freqBodyAccJerk-std()-X
+* freqBodyAccJerk-std()-Y
+* freqBodyAccJerk-std()-Z
+* "freqBodyGyro-mean()-X
+* freqBodyGyro-mean()-Y
+* freqBodyGyro-mean()-Z
+* freqBodyGyro-std()-X
+* freqBodyGyro-std()-Y
+* freqBodyGyro-std()-Z
+* freqBodyAccMag-mean()
+* freqBodyAccMag-std()
+* freqBodyBodyAccJerkMag-mean()
+* freqBodyBodyAccJerkMag-std()
+* freqBodyBodyGyroMag-mean()
+* freqBodyBodyGyroMag-std()
+* freqBodyBodyGyroJerkMag-mean()
+* freqBodyBodyGyroJerkMag-std()
+
+## Activities
+* WALKING (value = 1)
+* WALKING_UPSTAIRS (value = 2)
+* WALKING_DOWNSTAIRS (value = 3)
+* SITTING (value = 4)
+* STANDING (value = 5)
+* LAYING (value = 6)
+
+## Source
+The raw data for this project can be found at: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+An article describing the original project can be found at: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
---
+++ run_analysis.R
@@ -0,0 +1,62 @@
+run_analysis <- function() {
+    
+    ## Clean up
+    rm(list=ls())
+    
+    ## Load features (only care about 2nd col)
+    features <- read.table("features.txt")
+    features <- features[, 2]
+    
+    ## Clean up column names
+    features <- gsub("^t", "time", features)
+    features <- gsub("^f", "freq", features)
+    features <- gsub("BodyBody", "Bod", features)
+    
+    ## Build vector of column names with either "mean" or "std" in name
+    features_vector <- grepl("-mean\\(\\)|-std\\(\\)", features)
+    
+    ## Load activities (only care about 2nd col)
+    activities <- read.table("activity_labels.txt")
+    colnames(activities) <- c("activityID", "activity")
+    
+    ## Load test data sets
+    test.data <- read.table("test\\X_test.txt")
+    test.subject <- read.table("test\\subject_test.txt")
+    colnames(test.subject) <- c("subjectID")
+    test.activity <- read.table("test\\y_test.txt")
+    colnames(test.activity) <- c("activityID")
+    
+    ## Load train data sets
+    train.data <- read.table("train\\X_train.txt")
+    train.subject <- read.table("train\\subject_train.txt")
+    colnames(train.subject) <- c("subjectID")
+    train.activity <- read.table("train\\y_train.txt")
+    colnames(train.activity) <- c("activityID")
+    
+    ## set column names
+    colnames(test.data) <- features
+    colnames(train.data) <- features
+    
+    ## Combine
+    test.all <- cbind(test.data, subject = test.subject, activityID = test.activity, origin = "test")
+    train.all <- cbind(train.data, subject = train.subject, activityID = train.activity, origin = "train")
+    data.all <- rbind(test.all, train.all)
+    
+    ## Subset of data with 
+    data.subset <- data.all[features_vector]
+    
+    ## Perform merge with activites to get activity name
+    data.subset <- merge(data.subset, activities, by.x="activityID", by.y="activityID", all=TRUE)
+    
+    ## Remove un-needed cols
+    data.subset <- data.subset[, colnames(data.subset) != "activityID"]
+    data.subset <- data.subset[, colnames(data.subset) != "origin"]
+    
+    ## Reshape
+    data.melt <- melt(data.subset, id = c("activity", "subjectID"))
+    data.tidy <- dcast(data.melt, activity + subjectID ~ variable, mean)
+    
+    ## Write data
+    write.table(data.tidy, "r_analysis.txt", row.name = FALSE)
+    
+}
